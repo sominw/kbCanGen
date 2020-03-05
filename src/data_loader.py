89e4cfb1c374f9db150cfeb13c66146fb56c87b8
Somin Wadhwa <sominwadhwa@gmail.com>
2020-03-05 10:29:04 -0500

    reader

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,4 +1,4 @@
-from collections import OrderedDict
+afrom collections import OrderedDict
 from typing import List, Dict, OrderedDict
 import json
 
@@ -12,7 +12,13 @@ class ReadInput:
     def __init__(self, path, tokenizer: BertTokenizer, n_count, max_sent_size):
         
         data = json.load(open(path), object_pairs_hook=OrderedDict)
-        self.entity_ids = OrderedDict()
-        self.relation_ids = OrderedDict()
+        self.entity_dict = OrderedDict()
+        self.relation_dict = OrderedDict()
         self.entity_types = OrderedDict()
         self.relation_types = OrderedDict()
+        self.entity_types["None"] = EntityType("None", 0, "None")
+        self.entity_dict[0] = self.entity_types['None']
+        self.relation_types['None'] = RelationType("None", 0, "Negative")
+        
+        
+        
